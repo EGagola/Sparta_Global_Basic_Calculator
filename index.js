e15b9aad428fe901c350ecbdb80b61db891bd341
Elliot Gagola <egagola@spartaglobal.com>
2018-10-09 10:36:05 +0100

    Removed numbers from if statements to make code shorter and more resource friendly

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-// alert("Hello");
 var basicOrAdvanced = prompt("Would you like to use the basic functions or the advanced functions? (basic, advanced)").toLowerCase();
-if(basicOrAdvanced == "basic"){
+if(basicOrAdvanced == "basic") {
   var basicFunction = prompt("What function would you like to use? (+,-,*,/)");
+  var num1 = parseInt(prompt("Please enter the first number."));
+  var num2 = parseInt(prompt("Please enter the second number."));
   if (basicFunction == "+") {
-    var add1 = parseInt(prompt("Please enter the first number."));
-    var add2 = parseInt(prompt("Please enter the second number you would like to add to it."));
-    console.log(add1 + add2);
+    console.log(num1 + num2);
   }else if (basicFunction == "-") {
-    var sub1 = parseInt(prompt("Please enter the first number."));
-    var sub2 = parseInt(prompt("Please enter the second number you would like to subtract from it."));
-    console.log(sub1 - sub2);
+    console.log(num1 - num2);
   }else if (basicFunction == "*") {
-    var mult1 = parseInt(prompt("Please enter the first number."));
-    var mult2 = parseInt(prompt("Please enter the second number you would like to multiply it by."));
-    console.log(mult1 * mult2);
+    console.log(num1 * num2);
   }else if (basicFunction == "/") {
-    var div1 = parseInt(prompt("Please enter the first number."));
-    var div2 = parseInt(prompt("Please enter the second number you would like to divide it by."));
-    console.log(div1 / div2);
+    console.log(num1 / num2);
   }else {
     console.log("You have entered an invalid selection, please refresh the page.");
   }
 }else if (basicOrAdvanced == "advanced") {
   var advSelection = prompt("Which advanced function would you like to use? (square-root) or (square)").toLowerCase();
+    var num1 = prompt("Please enter the number.");
     if (advSelection == "square-root") {
-      var advSqrt = prompt("Please enter the number you wish to find the square root of.");
-      console.log(Math.sqrt(advSqrt));
+      console.log(Math.sqrt(num1));
     }else if (advSelection == "square") {
-      var advSqr = prompt("Please enter the number you wish to square.");
-      console.log(Math.pow(advSqr,2));
+      console.log(Math.pow(num1,2));
     }else {
       console.log("You have entered an invalid selection, please refresh the page.");
     }
